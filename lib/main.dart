56fa112d4bcc9f59c763ef7908d44d7475b5b0cc
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-06-17 18:44:03 -0400

    maso

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'tts.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:shake/shake.dart';
+import 'package:external_app_launcher/external_app_launcher.dart';
+import 'package:flutter_background/flutter_background.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
-void main() async{
+final androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: "flutter_background example app",
+    notificationText: "Background notification for keeping the example app running in the background",
+    notificationImportance: AndroidNotificationImportance.normal,
+    notificationIcon: AndroidResource(name: 'background_icon', defType: 'drawable'), // Default is ic_launcher from folder mipmap
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterBackground.initialize(androidConfig: androidConfig);
   runApp(const MyApp());
 }
 
@@ -28,15 +45,94 @@ class _MyHomePageState extends State<MyHomePage> {
   final stt.SpeechToText _speechToText = stt.SpeechToText();
   bool speechEnabled = false;
   String _lastWords = 'Testeo';
+  ShakeDetector? _detector;
+  Timer? _timer;
+  int _timerTotalSeconds = 0;
 
   @override
   initState() {
     super.initState();
     tts.init();
     _initSpeech();
+    enableBackgroundExecution();
+  } 
+
+  void enableBackgroundExecution() async {
+    bool success = await FlutterBackground.enableBackgroundExecution();
+    print(success);
+    _startDetector();
   }
 
-    void changedLanguageDropDownItem(String? selectedType) {
+  void _startDetector() {
+    print('init detector');
+    _detector?.stopListening();
+    _detector = ShakeDetector.autoStart(
+      onPhoneShake: onPhoneShake
+    );
+    count();
+  }
+
+  void count() async {
+    _timer = Timer(const Duration(seconds: 1), () {
+              _timerTotalSeconds += _timer!.tick;
+              final seconds = _timerTotalSeconds;
+              print('Background service alive ${seconds}s');
+              count();
+            });
+  }
+
+  void onPhoneShake(ShakeEvent event) async {
+          print('shaken!');
+          await LaunchApp.isAppInstalled(
+            androidPackageName: 'com.example.ihc_p3'
+          );
+          tts.newVoiceText = 'Quieres realizar algun comando de voz? Di confirmar.';
+          await tts.run();
+          await _speechToText.listen(
+            localeId: "es_ES",
+            listenOptions: stt.SpeechListenOptions(partialResults: false, cancelOnError: false),
+            onResult: (SpeechRecognitionResult result) {
+              print('Resultado: ${result.recognizedWords}');
+              if (result.recognizedWords.toLowerCase() == 'confirmar') { listenToOrder(); }
+              else { tts.newVoiceText = 'Comando cancelado.'; tts.run(); }
+            }
+          );
+  }
+
+  void listenToOrder() async {
+          tts.newVoiceText = 'Dicta tu comando.';
+          await tts.run();
+          await _speechToText.listen(
+            localeId: "es_ES",
+            listenOptions: stt.SpeechListenOptions(partialResults: false, cancelOnError: false),
+            onResult: (SpeechRecognitionResult result) async {
+              http.Response response = await http.post(Uri.parse("https://dialogflow.googleapis.com/v2/projects/newagent-tded/agent/sessions/62297da6-4429-cbe6-df0b-9d16f28a2dc8:detectIntent"), 
+              body: '''
+                    {
+                      "queryInput": 
+                      {
+                        "text":
+                        {
+                          "text": "${result.recognizedWords}",
+                          "languageCode": "es"
+                        }
+                      }
+                    }
+                    ''',
+              headers: {HttpHeaders.authorizationHeader: "Bearer ", HttpHeaders.contentTypeHeader: 'application/json'});
+              Map decoded = jsonDecode(utf8.decode(response.bodyBytes));
+              if (decoded["queryResult"]["action"] != 'toggle') {
+                tts.newVoiceText = 'DialogFlow no puede reconocer el comando: ${result.recognizedWords}';
+                await tts.run();
+              } else {
+                tts.newVoiceText = 'DialogFlow ha reconocido el item: "${decoded["queryResult"]["parameters"]["item"]}" y la acción: "${decoded["queryResult"]["parameters"]["lights-status"]}"';
+                await tts.run();
+              }
+            }
+          );
+  }
+
+  void changedLanguageDropDownItem(String? selectedType) {
     setState(() {
       tts.language = selectedType;
       tts.setLanguage(tts.language!);
@@ -79,6 +175,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void dispose() {
     super.dispose();
+    _detector?.stopListening();
     tts.stop();
   }
 
